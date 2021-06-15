0c0c0427ff53ef031cf26d9f114bd18aba988577
Thomas Seifert <t_ommy148@hotmail.com>
2021-06-14 23:54:17 -0500

    intial structure and thought process of password generator

--- script.js
+++ script.js
@@ -10,6 +10,58 @@ function writePassword() {
 
 }
 
+//intial function start for the var password from user inputs
+
+function generatePassword(){
+// string var of all different types of characters for selection 
+      var lowercase = "abcdefghijklmnopqrstuvwxyz"; 
+      var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      var numbers = "1234567890";
+      var specialCharacters = "!#$%&()*/:;<=>?@[]^{|}~";
+      var passwordResult = ""
+     
+// allows to set the parameters for the length of the password to be generated 
+      var passwordLength = prompt("Please enter a number between 8 and 128. This will be the number of characters in your password.");
+      while (passwordLength < 8 && passwordLength > 128) { passwordLength = prompt("Please enter a value between 8 and 128 only.")
+
+
+    }
+
+    //creating booleans for selected types of characters, which are also boxes on the screen to select your choice
+    var lwrcase = confirm("Would you like to use lower case letters?");
+
+    console.log(lwrcase);
+
+    if (lwrcase === true) {
+      passwordResult += lowercase;
+    };
+
+    var upCase = confirm("Would you like to use upper case letters?");
+    
+    console.log(upCase);
+
+    var nums = confirm("Would you like to use Numbers?");
+
+    console.log(nums);
+
+    var specChars = confirm("Would you like to use Special Characters?");
+
+    console.log(specChars);
+
+    //intial booleans created to start building possible selections for the password 6/14/21
+
+
+
+
+
+
+
+
+
+
+
+}
+
 
 
 
